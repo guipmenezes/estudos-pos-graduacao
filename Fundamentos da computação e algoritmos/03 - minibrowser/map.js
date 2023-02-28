d91219f5930a6273efbbe8b57d6100115c500bba
guipmenezes <guimenezesunb@gmail.com>
2023-02-28 12:22:23 -0300

    Add: Termina conteúdos aula 2

---
+++ Fundamentos da computação e algoritmos/03 - minibrowser/map.js
@@ -0,0 +1,10 @@
+var novoMapa = new Map();
+
+class HistoryMap {
+    
+}
+
+//11/11/2011 as 11 da noite
+novoMapa.set('11/11/2011 - 23:30 - URL', { title: 'Titulo do site', url: 'sozinha' });
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
